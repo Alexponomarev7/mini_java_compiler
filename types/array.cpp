c85431aae32c26c0e0903460191d93c0831294e3
lexolordan <lexolordan@yanex-team.ru>
2020-04-16 19:24:14 +0300

    fix arrays and make objects

---
+++ types/array.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alexey A. Ponomarev on 16.04.2020.
+//
+
+#include "array.h"
---
+++ types/array.h
@@ -0,0 +1,16 @@
+//
+// Created by Alexey A. Ponomarev on 16.04.2020.
+//
+
+#ifndef COMPILER_ARRAY_H
+#define COMPILER_ARRAY_H
+
+
+
+class array {
+
+};
+
+
+
+#endif //COMPILER_ARRAY_H
